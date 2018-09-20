db0b74a808a931976d8a09a0b5309b94ddf1d13e
00Zero0 <minatouzumakin001@gmail.com>
2018-09-20 09:38:18 +0545

    Bugs fixes in build script for linux

--- build_scripts/build_linux.sh
+++ build_scripts/build_linux.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Variables
-debug = ""
+let debug;
 
 # Detect arguments, if -release is present, use release compilation
 if [ "$1" == "-release" ]; then
-    debug = -D"NDEBUG"
+    debug="-D NDEBUG";
+fi
 
 # Make directories if not exists
 if [ ! -d ./../bin ]; then
@@ -25,5 +26,5 @@ if [ ! -d ./../bin/intermediates/proj_linux/Release/ ]; then
 fi
 
 # Compile files
-g++ -W"switch" -W"no-deprecated-declarations" -W"empty-body" -W"conversion" -W"return-type" -W"parentheses" -W"no-pointer-sign" -W"no-format" -W"uninitialized" -W"unreachable-code" -W"unused-function" -W"unused-value" -W"unused-variable" -std=c++11 -Wall -fno-strict-aliasing -g2 -gdwarf-2 debug -o "./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o" "./../src/linux/SZ_Socket_linux.cpp"
+g++ -W"switch" -W"no-deprecated-declarations" -W"empty-body" -W"conversion" -W"return-type" -W"parentheses" -W"no-format" -W"uninitialized" -W"unreachable-code" -W"unused-function" -W"unused-value" -W"unused-variable" -std=c++11 -Wall -fno-strict-aliasing -g2 -gdwarf-2 $debug -o "./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o" "./../src/linux/SZ_Socket_linux.cpp"
 ar rvs "./..bin/Release/SZ_Socket.a" "./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o"
