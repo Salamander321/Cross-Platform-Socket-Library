e7361f8a8176d6a2aef797529604388e0b34bdcb
00Zero0 <minatouzumakin001@gmail.com>
2018-09-20 10:43:42 +0545

    Build files simplefied

--- build_scripts/build_linux.sh
+++ build_scripts/build_linux.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-
-# Variables
-let debug;
-
 # Detect arguments, if -release is present, use release compilation
+echo "Running..."
 if [ "$1" == "-release" ]; then
-    debug="-D NDEBUG";
-fi
+  echo "Creating dreictories..."
+  $(mkdir -p "./../bin" "./../bin/Release" "./../bin/intermediates" "./../bin/intermediates/proj_linux" "./../bin/intermediates/proj_linux/Release")
 
-# Make directories if not exists
-if [ ! -d ./../bin ]; then
-  mkdir ./../bin;
-fi
-if [ ! -d ./../bin/Release/ ]; then
-  mkdir ./../bin/Release/;
-fi
-if [ ! -d ./../bin/intermediates ]; then
-  mkdir ./../bin/intermediates;
-fi
-if [ ! -d ./../bin/intermediates/proj_linux ]; then
-  mkdir ./../bin/intermediates/proj_linux;
-fi
-if [ ! -d ./../bin/intermediates/proj_linux/Release/ ]; then
-  mkdir ./../bin/intermediates/proj_linux/Release/;
-fi
+  echo "Compiling...(Release)"
+  $(g++ -W"switch" -W"no-deprecated-declarations" -W"empty-body" -W"conversion" -W"return-type" -W"parentheses" -W"no-format" -W"uninitialized" -W"unreachable-code" -W"unused-function" -W"unused-value" -W"unused-variable" -std=c++11 -Wall -fno-strict-aliasing -g2 -gdwarf-2 -D NDEBUG -o ./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o ./../src/linux/SZ_Socket_linux.cpp)
+  echo "Linking..."
+  $(ar rvs ./..bin/Release/SZ_Socket.a ./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o)
+  read -p "Press enter to continue"
 
-# Compile files
-g++ -W"switch" -W"no-deprecated-declarations" -W"empty-body" -W"conversion" -W"return-type" -W"parentheses" -W"no-format" -W"uninitialized" -W"unreachable-code" -W"unused-function" -W"unused-value" -W"unused-variable" -std=c++11 -Wall -fno-strict-aliasing -g2 -gdwarf-2 $debug -o "./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o" "./../src/linux/SZ_Socket_linux.cpp"
-ar rvs "./..bin/Release/SZ_Socket.a" "./../bin/intermediates/proj_linux/Release/SZ_Socket_linux.o"
+else
+  echo "Creating directories..."
+  $(mkdir -p "./../bin" "./../bin/Debug" "./../bin/intermediates" "./../bin/intermediates/proj_linux" "./../bin/intermediates/proj_linux/Debug")
+
+  echo "Compiling...(Debug)"
+  $(g++ -W"switch" -W"no-deprecated-declarations" -W"empty-body" -W"conversion" -W"return-type" -W"parentheses" -W"no-format" -W"uninitialized" -W"unreachable-code" -W"unused-function" -W"unused-value" -W"unused-variable" -std=c++11 -Wall -fno-strict-aliasing -g2 -gdwarf-2 -o ./../bin/intermediates/proj_linux/Debug/SZ_Socket_linux.o ./../src/linux/SZ_Socket_linux.cpp)
+  echo "Linking..."
+  $(ar rvs ./..bin/Debug/SZ_Socket.a ./../bin/intermediates/proj_linux/Debug/SZ_Socket_linux.o)
+  read -p "Press enter to continue"
+fi
